1962e1cf1409ba043c83a15cb7c503de56291be9
Md Aarif Raza <mdaarifraza03@gmail.com>
2024-09-24 13:36:29 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const SocketServer = require("./socketServer")
+const path = require('path')
 
 const app = express();
 app.use(express.json());
@@ -23,9 +24,10 @@ io.on("connection", (socket) => {
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Server is running..." });
-})
+
+// app.get("/", (req, res) => {
+//   res.json({ msg: "Server is running..." });
+// })
 
 // Routes
 app.use("/api", require("./routes/authRoute"));
@@ -51,6 +53,25 @@ mongoose
     console.log(`Connect Successfully`);
   })
   .catch(() => console.log(`Not Connected`))
+
+
+  // -----------------------------Deployment----------------
+
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV==="production") {
+  app.use(express.static(path.join(__dirname1,"/client/build")));
+
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
+  });
+
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// -----------------------------Deployment----------------
 
 const port = process.env.PORT || 8000;
 http.listen(port, () => console.log(`Sever is running on ${port}`)
