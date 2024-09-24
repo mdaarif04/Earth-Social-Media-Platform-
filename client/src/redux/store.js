bfc5ba9c7f3ed28797a8976efba96bf58b32c8d3
Md Aarif Raza <mdaarifraza03@gmail.com>
2024-09-24 17:43:55 +0530

    h

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,45 @@
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import {thunk} from "redux-thunk";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import rootReducer from './reducers/index.js'
+import { thunk } from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { Provider } from 'react-redux'
 
-import rootReducer from "./reducers/index";
+// import logger from "redux-logger";
 
-import { composeWithDevTools } from "redux-devtools-extension";
+// const store = createStore(rootReducer, 
+//   applyMiddleware(thunk,logger));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
-const DataProvider = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// )
+const DataProvider = ({children}) =>{
+  return(
+    <Provider store={store}>
+      {children}
+    </Provider>
+  )
+}
 
 export default DataProvider;
+
+// import { legacy_createStore as createStore, applyMiddleware } from "redux";
+// import rootReducer from "./reducers/index";
+// import { thunk } from "redux-thunk";
+// import { Provider } from "react-redux";
+// import logger from "redux-logger";
+// import { composeWithDevTools } from "redux-devtools-extension";
+
+// // Create the store with thunk and logger middleware, and integrate Redux DevTools
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk, logger))
+// );
+
+// const DataProvider = ({ children }) => {
+//   return <Provider store={store}>{children}</Provider>;
+// };
+
+// export default DataProvider;
